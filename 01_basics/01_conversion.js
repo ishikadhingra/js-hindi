d2651e423ed2407ee47e9bf4ee48cdaa0e236d13
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-12 08:53:20 +0000

    config done

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,15 @@
+ let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log( valueInNumber);
+
+/*
+33 -> 33
+33abc -> Nan 
+true -> 1
+null -> 0
+
+*/
+
