9b3a2c9804ac96856361c5b21e0df2c3e4b4c35a
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-13 04:55:33 +0000

    container config done

---
+++ 01_basics/operation.js
@@ -0,0 +1,12 @@
+let str1= "Ishika";
+let str2 = " Dhingra";
+
+let str3 = str1 + str2 ;
+console.log(str3);
+//  string concatenation
+console.log(null > 0)
+console.log(null == 0)
+
+console.log(null <= 0)
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,8 @@
+const name = "ishika";
+const repocount = 50;
+console.log(`my name is ${name} and my repocount is ${repocount}`);
+const gameName =  new String("ishika2")
+console.log(gameName)
+const url="https://mdicopper.com/ishika%20dhingra"
+
+console.log(url.replace('%20' , '_'))
