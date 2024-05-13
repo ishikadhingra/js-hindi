4ebc91e8147684a7d686d454b2289949f218c252
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-13 04:52:29 +0000

    container config done

---
+++ 01_basics/number.js
@@ -0,0 +1,13 @@
+const score = 33;
+// console.log(score);
+const balance = new Number(200)
+// console.log(balance.toFixed(6));
+
+const othernumer = 23.0404;
+// console.log(othernumer.toPrecision(5))
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-In'))
+
+// math method
+console.log(Math.abs(-4));
+console.log(Math.sqrt(25))
