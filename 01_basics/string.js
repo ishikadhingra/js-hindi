226a1b4a00828d6ca4077ef9e6556a70615375d2
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-14 11:48:59 +0000

    container config done

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,8 +1,17 @@
 const name = "ishika";
 const repocount = 50;
-console.log(`my name is ${name} and my repocount is ${repocount}`);
+// console.log(`my name is ${name} and my repocount is ${repocount}`);
 const gameName =  new String("ishika2")
-console.log(gameName)
+// console.log(gameName)
 const url="https://mdicopper.com/ishika%20dhingra"
 
-console.log(url.replace('%20' , '_'))
+// console.log(url.replace('%20' , '_'))
+
+const link = new String ("ishika_dhingr_a")
+// console.log(link.__proto__)
+// console.log(link.length)
+// console.log(link.toLocaleLowerCase())
+// console.log(link.charAt(0))
+// console.log(link.indexOf('m'))
+// console.log(link.substring(0,7))
+console.log(link.split('_'))
