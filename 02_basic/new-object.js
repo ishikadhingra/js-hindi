2917fdb3896ae204d249a5d39f880e53534dd5ae
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-25 14:49:11 +0000

    container config done

---
+++ 02_basic/new-object.js
@@ -0,0 +1,10 @@
+// destructured krna object ko 
+const obj ={
+    course: "isha",
+    price: 999,
+    teacher:"sahil",
+}
+// old method obj.price
+// new  method to print values 
+const {course} = obj
+console.log(course)
