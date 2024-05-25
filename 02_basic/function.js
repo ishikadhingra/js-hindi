5a04f58f7ea28e328755fcda97952903a82cc170
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-25 16:51:29 +0000

    container config done

---
+++ 02_basic/function.js
@@ -0,0 +1,31 @@
+function ishika (){
+// console.log("i");
+// console.log("s");
+// console.log("h");
+// console.log("i");
+// console.log("k");
+// console.log("a");
+
+}
+ishika();
+
+function addTwoNumbers (number1, number2){
+    // these are the parameters
+// console.log(number1 + number2)
+
+
+}
+addTwoNumbers(2, null)
+// these are the argument jb function call krna h 
+// 2 method
+function addTwoNumbers (number1, number2){
+
+    return (number1+number2)
+}
+const result = addTwoNumbers(2, 10)
+// console.log("Result : ", result);
+// rest and spread operations
+function calculateAddCart(val1, val2, ...num1){
+return (num1)
+}
+console.log(calculateAddCart(2, 300, 400, 20000));
