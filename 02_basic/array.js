e02e5ecd0e7a0905b17f0438d9925c4c9a973073
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-05-25 14:51:01 +0000

    container config done

---
+++ 02_basic/array.js
@@ -0,0 +1,13 @@
+// array in js 
+const myarr = [1, 2, 3, "isha", 5, ];
+//  console.log(myarr[4]);
+ // second method to declare array
+ const myarr2 = new  Array(1, 2, 3, 4, 5,);
+//  console.log(myarr2[2]);
+ // array method  pop, push , shift, unshift, includes, index, join, flat, from , of , isarray
+//   myarr.shift();
+//   console.log(myarr) 
+// console.log(myarr.indexOf(4))
+const newarr = myarr .join()
+// console.log(typeof(newarr));
+console.log(myarr);
