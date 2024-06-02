24af75742de8ff948fb4a190173da7029ac91287
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2024-06-02 15:06:34 +0000

    container config done

---
+++ 03_scope/scope.js
@@ -0,0 +1,21 @@
+var b = 300;
+if(true){
+    let a = 4;
+    var b = 3;
+    const c= 6;  
+
+   
+}
+
+// console.table([b]);
+// console.log(one(7));
+function one(num){
+return num +1
+}
+
+
+const add = function (num){
+return num +2
+}
+console.log(add(10))
+// hoisting in js function ko variable m hold krana
