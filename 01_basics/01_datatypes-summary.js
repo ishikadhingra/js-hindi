be9b30af53ac81e1c1980547e86bf6c4b0947828
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-24 13:47:21 +0000

    config done

--- 01_basics/01_datatypes-summary.js
+++ 01_basics/01_datatypes-summary.js
@@ -4,20 +4,36 @@
 // arrays, objects, functions
 const score  =100
 const scoreValue = 100.3
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
-const id= Symbol('123')
-const anotherId= Symbol('123')
-console.log(id===anotherId)
-const bigNumber = 4848484848483448n
-const hero= ["isha", "popy", "sahil"]
-let myObj={
-    name:"isha",
-    age:22,
-}
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id= Symbol('123')
+// const anotherId= Symbol('123')
+// console.log(id===anotherId)
+// const bigNumber = 4848484848483448n
+// const hero= ["isha", "popy", "sahil"]
+// let myObj={
+//     name:"isha",
+//     age:22,
+// }
 
-const myfunction = function(){
-console.log("helloworld");
+// const myfunction = function(){
+// console.log("helloworld");
+// }
+// console.log(typeof anotherId);
+//stack and heap memory
+// stack (primitive), heap (non-primitive)
+// copy and reference original values
+let myName = "ishikadhingra9924@gmail.com"
+let anotherName = myName
+anotherName = "sahilkashyap"
+// console.log(myName);
+// console.log(anotherName);
+let userOne = {
+    email:"user@gmail.com",
+    name:"isha"
 }
-console.log(typeof anotherId);
+let userTwo = userOne
+userTwo.email="sahil@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
