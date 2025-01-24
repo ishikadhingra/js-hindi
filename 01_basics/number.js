ec1b8d62364cd20e33f25ce874ce5a79a397c12d
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-24 18:23:43 +0000

    config done

--- 01_basics/number.js
+++ 01_basics/number.js
@@ -1,13 +1,19 @@
-const score = 33;
+const score = 400
 // console.log(score);
-const balance = new Number(200)
-// console.log(balance.toFixed(6));
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otherNumber = 123.8966
 
-const othernumer = 23.0404;
-// console.log(othernumer.toPrecision(5))
-const hundreds = 100000;
+// console.log(otherNumber.toPrecision(4));
+// const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-In'))
-
-// math method
-console.log(Math.abs(-4));
-console.log(Math.sqrt(25))
+//maths
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.5));
+// console.log(Math.max(3,4,9,40));
+// console.log(Math.min(3,4,9,40));
+console.log(Math.random());
