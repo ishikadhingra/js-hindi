45d9e720526ce83f6ece787af31a58f5a3e082c7
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-24 10:04:06 +0000

    container config done

--- 01_basics/01_conversion.js
+++ 01_basics/01_conversion.js
@@ -1,15 +1,17 @@
- let score = "33abc"
+let score = "33abc";
 // console.log(typeof score);
-// console.log(typeof(score))
-
-let valueInNumber= Number(score)
-console.log( valueInNumber);
-
-/*
-33 -> 33
-33abc -> Nan 
-true -> 1
-null -> 0
-
-*/
-
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// // "33" = > 33
+// "33abc" => nan
+// true =>1;
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) ;
+//1=> true; 0=> false
+// "" => false
+//"isha" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
