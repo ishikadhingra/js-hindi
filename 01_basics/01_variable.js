959ef30898a34f4b76a9e6373ce22ba5f1c8216b
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-24 09:23:13 +0000

    container config done

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,14 +1,16 @@
-accountId = 49494949;
-mail="ishikadhingra9924@gmail.com";
-city= "gzb;"
-password = 72728;  
-
-console.table([accountId, mail, city, password,]);
-
-    
-
+const accountId = 184848
+let accountEmail = "ishikadhingra9924@gmail.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2// not allowed
+accountEmail = "sahil01@gmail.com"
+accountPassword = "89t5r89"
+accountCity = "pune"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
 /*
-prefer not to use var in block scope & functional scope; 
+prefer not to use var because
+of issue in block scope and functional scope
+
 */
-
-
