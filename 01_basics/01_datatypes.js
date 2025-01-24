08379d10a70aac2f907f2b343281172f1c15f182
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-24 09:44:33 +0000

    container config done

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,38 +1,16 @@
-// alert("3+3");
-//  console.log(alert); we  are using node js not browser.
-
-// use strict use as a code new version
-
-
-// this is node js not in browser
-
-// let age = 19;
-//nunber 12344
-// bigint
-// null empty
-// boolean
-// string
-// null 
-// undefined means not declaring a value
-// symbol  unique these are primitive datatype
-// object non primitive
-console.log(typeof undefined);
-
-
-// stack primitive -->  heap -> non primitive
-let ishika = "text";
-let ishika2= ishika;
-ishika2= "dhingra";
-// console.log(ishika);
-// console.log(ishika2);
-
-
-
-let userone={
-    mail:"ishika@gmail.com",
-    phone: 283838,
-}
-let  usertwo= userone;
-userone.mail = "dhingra@gmail.com";
-console.log(userone.mail)
-console.log(usertwo.mail)
+"use strict"; // treat akk JS code as newer version
+// alert (3+3)// we are using node js , not browser
+let name  = "ishika"
+let age = 23
+let isLoggedIn = false 
+//number => 2 to power 53
+// bigInt
+// string =>""
+// boolean =>true/false
+//null=> standalone value
+// undefined => value assign ni hui h 
+// symbol=> unique
+//object
+console.log(typeof age);
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
