7cae99535f5e0b1ec95c1e9ce6b621fb4894d815
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-28 14:24:40 +0000

    config done

--- 02_basic/newarray.js
+++ 02_basic/newarray.js
@@ -1,7 +1,7 @@
 const marver_heros = ["ironman", "spiderman", "captainamerica"];
 const bollywood_heros = ["srk", "rk", "ak", "hr"]
-marver_heros.push(bollywood_heros);
-// console.log(marver_heros);
+const allHeroes= [...marver_heros,...bollywood_heros];
+console.log(allHeroes);
 const anotherarr= [1, 2, [3], [4, 5,], [4, 8 , 9 , 0] ]
 const realarr= anotherarr.flat(Infinity);
-console.log(realarr);
+// console.log(realarr);
