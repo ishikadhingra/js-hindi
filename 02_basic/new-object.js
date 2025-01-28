f28d4566179377ef835416f9528c13ffadb61c2c
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-28 18:25:47 +0000

    config done

--- 02_basic/new-object.js
+++ 02_basic/new-object.js
@@ -1,10 +1,19 @@
-// destructured krna object ko 
-const obj ={
-    course: "isha",
-    price: 999,
-    teacher:"sahil",
+// const instaUser = new Object() constructor singleton
+// 2 method object literals
+const insta_user = {
+
 }
-// old method obj.price
-// new  method to print values 
-const {course} = obj
-console.log(course)
+insta_user.id =12
+insta_user.name = "Ishika"
+insta_user.isLoggedIn = false
+
+
+
+// console.log(insta_user);
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"a", 4:"b"}
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
+/* object.keys and object.values
+ kjkjj
+*/
