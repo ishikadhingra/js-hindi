2368ea0ad6abb7d1a1af51737d53a5ba96f61656
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-28 07:47:01 +0000

    config done

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -1,13 +1,22 @@
 // array in js 
-const myarr = [1, 2, 3, "isha", 5, ];
-//  console.log(myarr[4]);
- // second method to declare array
- const myarr2 = new  Array(1, 2, 3, 4, 5,);
-//  console.log(myarr2[2]);
- // array method  pop, push , shift, unshift, includes, index, join, flat, from , of , isarray
-//   myarr.shift();
-//   console.log(myarr) 
-// console.log(myarr.indexOf(4))
-const newarr = myarr .join()
-// console.log(typeof(newarr));
-console.log(myarr);
+const myArr= [0,1,2,3,4,5]
+// console.log(myArr[2]);
+const myArr2 = new Array(1,2,3,4,5)
+//array method
+// myArr.push()
+// myArr.pop()
+// myArr.unshift(8)
+// myArr.shift(8)
+// console.log(myArr.includes(9));
+// console.log(myArr);
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// slice,splice
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B", myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C", myArr);
