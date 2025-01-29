44a6606f2fb6932d60fa73acd166c529d7c8a799
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-01-29 13:02:26 +0000

    config done

--- 02_basic/function.js
+++ 02_basic/function.js
@@ -1,31 +1,29 @@
 function ishika (){
-// console.log("i");
-// console.log("s");
-// console.log("h");
-// console.log("i");
-// console.log("k");
-// console.log("a");
+console.log("i");
+console.log("s");
+console.log("h");
+console.log("i");
+console.log("k");
+console.log("a");
 
 }
-ishika();
+// ishika();
 
+// function addTwoNumbers (number1, number2){
+// console.log(number1+number2)
+// }
 function addTwoNumbers (number1, number2){
-    // these are the parameters
-// console.log(number1 + number2)
-
-
+    // console.log("ishika")
+    return number1+number2
 }
-addTwoNumbers(2, null)
-// these are the argument jb function call krna h 
-// 2 method
-function addTwoNumbers (number1, number2){
-
-    return (number1+number2)
+const result = addTwoNumbers(3,4)
+// console.log("Result", result);
+function loginUserMessage (username){
+    
+    return `${username} just logged in`
 }
-const result = addTwoNumbers(2, 10)
-// console.log("Result : ", result);
-// rest and spread operations
-function calculateAddCart(val1, val2, ...num1){
-return (num1)
+// console.log(loginUserMessage())
+function calculateCartPrice(...num1){
+return num1
 }
-console.log(calculateAddCart(2, 300, 400, 20000));
+console.log(calculateCartPrice(200, 300, 400))
