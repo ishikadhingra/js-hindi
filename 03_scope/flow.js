3e1e68431c4d7c8a760bee72059fb3f9aeeb9c6d
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-03 19:20:49 +0000

    config done

---
+++ 03_scope/flow.js
@@ -0,0 +1,28 @@
+// const temp = 41
+// if(temp === 40){
+//     console.log("less than 50");
+    
+// }else{
+//     console.log("temperature is greator than 50 ");
+
+// }
+// <,>,==, ===, !=
+// const score = 200
+// if(score >100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+//else if 
+const userLoggedIn = true
+const debitcard = true
+const loggedInfromGoogle = true
+const loggedInfromEmail = false
+if(userLoggedIn && debitcard && 2==3){
+console.log("allow to buy course");
+
+}
+if(loggedInfromGoogle || loggedInfromEmail){
+console.log("User logged in")
+}
