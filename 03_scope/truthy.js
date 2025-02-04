b84dd2ccd6cbbdc210ec0bdca499392f6a062f5a
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-04 14:52:17 +0000

    config done

---
+++ 03_scope/truthy.js
@@ -0,0 +1,20 @@
+// const userEmailId = []
+// if(userEmailId){
+// console.log("got user email");
+// }else{
+//     console.log("don't have user email");
+
+// }
+// falsy values , 0, -0, BigInt 0n, "", null, undefined,NAN
+// true values , "0", "false", " ", [],{}, function(){}
+// Nullish Coalescing Operator (??) : null undefined
+let val1 ;
+// val1 = 5?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+// console.log(val1);
+// ternary operator
+// condition  ? true:false
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("less tha 80"): console.log("greator than 80")
