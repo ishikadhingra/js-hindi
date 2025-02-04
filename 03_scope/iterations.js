9bf3b434f39b8dbfd37ef669073f2162e964a437
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-04 18:25:35 +0000

    config done

---
+++ 03_scope/iterations.js
@@ -0,0 +1,28 @@
+// for
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if(element ==5){
+// console.log("5 is best number");
+    }
+    
+    // console.log(element);
+
+}
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for(let j=0; j<= 10; j++){
+        // console.log(`Inner loop value is ${j} and outer loop value is ${i}`);
+    }
+}
+// let myArray = ["isha", "sahil", "sahisha"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+//break and continue
+for (let index = 0; index <= 20; index++) {
+if(index ==5){
+    console.log(`detected 5`)
+continue
+    }    console.log(`value of i is ${index}`)    
+}
