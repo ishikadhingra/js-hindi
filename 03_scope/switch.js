c243e5d0e0603c7c8f3051bffed9bc401e3365c2
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-04 14:28:19 +0000

    config done

---
+++ 03_scope/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//break kwyword is use to control the flow of statement
+const month = 2
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
