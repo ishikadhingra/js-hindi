10ab404fd8cdfb6d9d1c9635c39b1e2b69ac6bd8
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-04 18:51:16 +0000

    config done

---
+++ 03_scope/loop.js
@@ -0,0 +1,10 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`)
+// index = index+2
+// }
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
