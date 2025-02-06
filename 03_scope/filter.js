5cf4451250eeb84d759f9413eb88e6ad25c58288
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-06 07:48:51 +0000

    config done

---
+++ 03_scope/filter.js
@@ -0,0 +1,9 @@
+const myNums = [1,2,3,4,5,6,7,8,9]
+const newNums = myNums.filter((num)=>{
+    return num>4;
+})
+console.log(newNums);
+//for each return ni krta value 
+//filter return krta h 
+//explicit return  {}return
+// implicit return ()=> 
