ff5dd70d1381f75f9fec6bc821159a8b3a10b8d2
Ishika Dhingra <82260404+ishikadhingra@users.noreply.github.com>
2025-02-06 06:39:23 +0000

    config done

---
+++ 03_scope/for-in.js
@@ -0,0 +1,14 @@
+const  myObject= {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+for (const key in  myObject) {
+//   console.log(myObject[key]);
+  
+}
+const programming = ["js","rb","py","java","cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+}
